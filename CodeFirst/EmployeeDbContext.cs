6f958fe4ee40949817d30a86dbe2b75d2dafae87
Ramya Vempalla <Ramya.Vempalla@neudesic.com>
2018-07-21 16:53:34 +0530

    Code first approach

---
+++ CodeFirst/EmployeeDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CodeFirst.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public DbSet<EmployeeModel> EmployeeDetails { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost; Database=Employees; Trusted_Connection=True; MultipleActiveResultSets=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
